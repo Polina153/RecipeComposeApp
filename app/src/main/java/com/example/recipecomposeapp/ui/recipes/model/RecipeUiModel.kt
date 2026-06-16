5a2a41db82959e9890e7f60831a180d57e5390d7
Linka <Polina153@mail.ru>
2026-06-16 16:00:58 +0400

    Замечания учтены

--- app/src/main/java/com/example/recipecomposeapp/ui/recipes/model/RecipeUiModel.kt
+++ app/src/main/java/com/example/recipecomposeapp/ui/recipes/model/RecipeUiModel.kt
@@ -2,7 +2,6 @@ package com.example.recipecomposeapp.ui.recipes.model
 
 import androidx.compose.runtime.Immutable
 import com.example.recipecomposeapp.Constants
-import com.example.recipecomposeapp.data.model.IngredientDto
 import com.example.recipecomposeapp.data.model.RecipeDto
 
 @Immutable
@@ -10,7 +9,7 @@ data class RecipeUiModel(
     val id: Int,
     val title: String,
     val imageUrl: String,
-    val ingredients: List<IngredientDto>,
+    val ingredients: List<IngredientUiModel>,
     val method: List<String>,
     val isFavorite: Boolean
 )
@@ -19,7 +18,7 @@ fun RecipeDto.toUiModel(): RecipeUiModel {
     return RecipeUiModel(
         id = id,
         title = title,
-        ingredients = ingredients,
+        ingredients = ingredients.map { it.toUiModel() },
         method = method,
         imageUrl =
             if (imageUrl.startsWith("http")) imageUrl else Constants.ASSETS_URI_PREFIX + imageUrl,
